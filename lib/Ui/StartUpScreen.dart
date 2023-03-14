16dbac1bf0558c98f9904d3f46189b179b9d77e1
sanin <saninsnz@gmail.com>
2023-03-14 11:35:54 +0530

    Initial commit

---
+++ lib/Ui/StartUpScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class StartUpScreen extends StatelessWidget {
+  const StartUpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+
+    );
+  }
+}
